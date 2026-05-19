b34bb7feb04e90f444340c1adc21d62fd1bfb1bb
chirag jain <jchirag0610@gmail.com>
2026-05-20 01:26:15 +0530

    axios api calling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import { useEffect } from 'react';
+import axios from '../utils/axios';
 
 const Home = () => {
+
+  const getRecipes = async () => {
+    try {
+      const { data } = await axios.get("/recipes");
+      console.log(data);
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
   return (
     <div>Home</div>
   )
